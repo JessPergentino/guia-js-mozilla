0d4e13fdf5cbaaab1aeda2d41e501607a2795fda
Jessica Pergentino <jessicapergentino@hotmail.com>
2019-01-17 21:43:51 -0300

    Praticas do projeto

--- modulo5/artigo4-9/express-locallibrary-tutorial/app.js
+++ modulo5/artigo4-9/express-locallibrary-tutorial/app.js
@@ -6,8 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://mongodb:Mx6IsSHt6xY21abF@cluster1-shard-00-00-merhy.mongodb.net:27017,cluster1-shard-00-01-merhy.mongodb.net:27017,cluster1-shard-00-02-merhy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
